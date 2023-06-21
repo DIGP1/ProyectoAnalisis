2107e8675cdf577a5a08e5880df86fc139936fb3
AngeLSanchez210 <is21001@ues.edu.sv>
2023-06-21 10:13:41 -0600

    error

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from . import views
 
@@ -12,15 +11,4 @@ urlpatterns = [
     path('cambiar', views.cambiar),
     path('cambiarcontra', views.cambiarcontra)
     
-=======
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('pruebas', views.buscar),
-    path('Registro',views.registrarUsuario),
-    path('inicioSesion', views.inicioSesion),
-    path('cerrarSesion', views.cerrarSesion)
->>>>>>> adea497916b6fbb42bf4fdedd9214626e0f16cf5
 ]
